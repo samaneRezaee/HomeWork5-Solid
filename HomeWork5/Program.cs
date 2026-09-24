e63dbf21f981ffc3e21feac2d210e347c73b5081
sam_Rezaee <srezaie.c.e@gmail.com>
2026-09-24 22:34:37 +0330

    write task2 part in main method

--- HomeWork5/Program.cs
+++ HomeWork5/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace HomeWork5
+﻿using HomeWork5.Task2.Base;
+using HomeWork5.Task2.High_level_Module;
+using HomeWork5.Task2.Implementations;
+
+namespace HomeWork5
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Test task2-----------------------");
+            // for DB
+            IProductRepository dbRepo = new DatabaseProductRepository();
+            ProductDisplayManager managerWithDb = new ProductDisplayManager(dbRepo);
+            managerWithDb.ShowProducts();
+
+            // for Api
+            IProductRepository apiRepo = new ApiProductRepository();
+            ProductDisplayManager managerWithApi = new ProductDisplayManager(apiRepo);
+            managerWithApi.ShowProducts();
+
+            // for File
+            IProductRepository fileRepo = new FileProductRepository();
+            ProductDisplayManager managerWithFile = new ProductDisplayManager(fileRepo);
+            managerWithFile.ShowProducts();
         }
     }
 }
