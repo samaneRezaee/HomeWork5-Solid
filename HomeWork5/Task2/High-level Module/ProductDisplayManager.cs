993ab282ae260194479d3804fb00a831e6464d99
sam_Rezaee <srezaie.c.e@gmail.com>
2026-09-24 22:30:15 +0330

    write Implementation layer classes for Api, DB and File write productDisplayManager class in High-level

---
+++ HomeWork5/Task2/High-level Module/ProductDisplayManager.cs
@@ -0,0 +1,30 @@
+﻿using HomeWork5.Task2.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5.Task2.High_level_Module
+{
+    public class ProductDisplayManager
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductDisplayManager(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void ShowProducts()
+        {
+            var products = _repository.GetAllProducts();
+            Console.WriteLine("Displaying Product List:");
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.ToString());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HomeWork5/Task2/Implementations/DatabaseProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using HomeWork5.Task2.Base;
+using HomeWork5.Task2.Domain_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5.Task2.Implementations
+{
+    public class DatabaseProductRepository : IProductRepository
+    {
+        public List<Product> GetAllProducts()
+        {
+            Console.WriteLine("--Fetching data from Database --");
+            return new List<Product>()
+            {
+                new Product { Id = 1, Name = "DB Laptop", Price = 2000},
+                new Product { Id = 2, Name = "DB Mouse", Price = 100}
+            };
+        }
+    }
+}
